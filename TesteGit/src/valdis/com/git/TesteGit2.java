b950d60219da20eddf3d8255f2035ab0f18ddc17
Valdisnei Fajardo <valdisnei.fajardo@gmail.com>
2013-08-18 00:12:13 -0300

    Teste GIT java

---
+++ TesteGit/src/valdis/com/git/TesteGit2.java
@@ -0,0 +1,10 @@
+package git;
+
+public class TesteGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+
+	}
+
+}
---
+++ TesteGit/src/valdis/com/git/ValdisTeste.java
@@ -0,0 +1,11 @@
+package git;
+
+public class ValdisTeste {
+
+	public static void main(String[] args) {
+		System.out.println("Teste GIT");	
+		
+
+	}
+
+}
